586f8ccb4794564ce87e81f35e62e49903cf635d
Matej Bačo <matejbaco2000@gmail.com>
2025-01-03 10:08:08 +0100

    Add old

--- src/routes/logs/+page.server.ts
+++ src/routes/logs/+page.server.ts
@@ -1,25 +1,9 @@
 /** @type {import('./$types').PageLoad} */
-import * as fs from "fs";
-
-let i = 0;
 
 export function load() {
-	let y;
-
-	if (!fs.existsSync("/tmp/l.txt")) {
-		y = 0;
-	} else {
-		y = parseInt(fs.readFileSync("/tmp/l.txt").toString());
-	}
-
-	i++;
-	y++;
-
-	console.log("A log printed " + i + " times and " + y + " global times " + Date.now());
-
-	fs.writeFileSync("/tmp/l.txt", y.toString());
+	let x = process.env.OLD
 
 	return {
-		msg: "All logs printed"
+		msg: "Env: " + x
 	};
 }
